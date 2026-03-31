a6ae1096ab5f2eb23b7aa683131b0b10a87825d5
Kanak sharma <sharmakanak043@gmail.com>
2026-04-01 00:32:45 +0530

    fix: update geist imports to match official docs

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,12 @@
 import { GeistSans } from "geist/font/sans";
 import { GeistMono } from "geist/font/mono";
+import { 
+  GeistPixelSquare, 
+  GeistPixelGrid, 
+  GeistPixelCircle, 
+  GeistPixelTriangle, 
+  GeistPixelLine 
+} from "geist/font/pixel";
 import "./globals.css";
 
 const geistSans = GeistSans;
